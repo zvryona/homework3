51e9fe9671e271855d8b1e3b3ae2c85e48c0659b
alena <little.eblan@gmail.com>
2023-03-03 21:14:37 +0500

    начальный ввод

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,65 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+
+        int q = 200000;
+        byte w = 10;
+        short e = 30000;
+        long r = 1000000000L;
+        float t = 45.5f;
+        double u = 5.15161516;
+        System.out.println("Значение переменной q  с типом int равно " + q );
+        System.out.println("Значение переменной w  с типом byte равно " + w );
+        System.out.println("Значение переменной e с типом short равно " + e );
+        System.out.println("Значение переменной r  с типом long равно " + r );
+        System.out.println("Значение переменной  t с типом float равно " + t );
+        System.out.println("Значение переменной u с типом double равно " + u );
+
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+
+    }
+
+    public static void task3 () {
+        System.out.println("Задача 3");
+
+    }
+
+    public static void task4 () {
+        System.out.println("Задача 4");
+
+    }
+
+    public static void task5 () {
+        System.out.println("Задача 5");
+
+    }
+
+    public static void task6 () {
+        System.out.println("Задача 6");
+
+    }
+
+    public static void task7 () {
+        System.out.println("Задача 7");
+
+    }
+
+    public static void task8 () {
+        System.out.println("Задача 8");
+
     }
 }
